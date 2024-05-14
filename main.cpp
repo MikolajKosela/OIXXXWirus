2f2c289048fdeea4859e800f0ca81c197c9586bf
Mikołaj Kosela <mikolaj09521@gmail.com>
2024-05-14 21:20:35 +0200

    Program z vektorem

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
+#include<vector>  
 using namespace std;
+
+bool XOR (bool a, bool b)
+{
+    if(a==b) return 0;
+    else return 1;
+}
 int main()
 {
-
-
+    int n;
+    cin>>n;
+    vector<bool>tab;
+    for(int i=0; i<n; i++)
+    {
+        int a; cin>>a;
+        tab.push_back(a);
+    }
+    vector<bool>::iterator pom = tab.begin();
+    cout<<*pom;
     return 0;
 }
